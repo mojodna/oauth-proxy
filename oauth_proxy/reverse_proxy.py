b9dd4c2af345550bf2e1a30f1332347fe25da7ae
Seth Fitzsimmons <seth@mojodna.net>
2008-04-26 12:14:14 -0400

    prevent name conflicts

--- oauth_proxy/reverse_proxy.py
+++ oauth_proxy/reverse_proxy.py
@@ -7,12 +7,12 @@ class Options(usage.Options):
 	synopsis = "Usage: oauth_reverse proxy --remote-host <remote host> [--remote-port <remote port>] [--path-prefix <path prefix>] [-p <proxy port>] [--ssl] [--ssl-private-key <private key] [--ssl-certificate <certificate>]"
 	longdesc = "Makes an OAuth reverse HTTP proxy server.."
 	optParameters = [
-	    ['path-prefix',     None, '',   "Path prefix"],
-		['port', 'p',       8080, "Proxy port", int],
-	    ['remote-host',     None, None, "Remote host"],
-	    ['remote-port',     None, 80,   "Remote port"],
-	    ['ssl-certificate', None, None, "SSL certificate"],
-	    ['ssl-private-key', None, None, "SSL private key"],
+		['path-prefix',		None, '',	"Path prefix"],
+		['port', 'p',		8080, "Proxy port", int],
+		['remote-host',		None, None, "Remote host"],
+		['remote-port',		None, 80,	"Remote port"],
+		['ssl-certificate', None, None, "SSL certificate"],
+		['ssl-private-key', None, None, "SSL private key"],
 	]
 
 	optFlags = [['ssl', 's']]
@@ -20,54 +20,54 @@ class Options(usage.Options):
 
 
 class OAuthValidator:
-    def validate(self):
-        """Validate an OAuth request"""
-        return True
+	def validate(self):
+		"""Validate an OAuth request"""
+		return True
 
 
 
 class OAuthReverseProxyRequest(proxy.ReverseProxyRequest):
-    # TODO this class may be unnecessary if header rewriting can occur in OAuthReverseProxyResource
-        
-    proxyClientFactoryClass = ProxyClientFactory
+	# TODO this class may be unnecessary if header rewriting can occur in OAuthReverseProxyResource
+		
+	proxyClientFactoryClass = ProxyClientFactory
 
-    def __init__(self, validator, *args):
-        self.validator = validator
-        proxy.ReverseProxyRequest.__init__(self, *args)
+	def __init__(self, validator, *args):
+		self.validator = validator
+		proxy.ReverseProxyRequest.__init__(self, *args)
 
 
-    def process(self):
-        # This logic either goes here or in OAuthReverseProxyResource.render
-        
-        # filter querystring from self.uri
-        
-        # filter headers from self.getAllHeaders()
-        
-        # validate oauth params
-        valid = True
-        
-        if valid:
-            proxy.ReverseProxyRequest.process(self)
-            # looks like: 
-            # self.received_headers['host'] = self.factory.host
-            # clientFactory = self.proxyClientFactoryClass(
-            #     self.method, self.uri, self.clientproto, self.getAllHeaders(),
-            #     self.content.read(), self)
-            # self.reactor.connectTCP(self.factory.host, self.factory.port,
-            #                         clientFactory)
-        else:
-            # return an error message
-            pass
+	def process(self):
+		# This logic either goes here or in OAuthReverseProxyResource.render
+		
+		# filter querystring from self.uri
+		
+		# filter headers from self.getAllHeaders()
+		
+		# validate oauth params
+		valid = True
+		
+		if valid:
+			proxy.ReverseProxyRequest.process(self)
+			# looks like: 
+			# self.received_headers['host'] = self.factory.host
+			# clientFactory = self.proxyClientFactoryClass(
+			#	  self.method, self.uri, self.clientproto, self.getAllHeaders(),
+			#	  self.content.read(), self)
+			# self.reactor.connectTCP(self.factory.host, self.factory.port,
+			#						  clientFactory)
+		else:
+			# return an error message
+			pass
 
 
 
 class OAuthReverseProxy(proxy.ReverseProxy):
 
-    # TODO this may be the only required line, if validation occurs in OAuthReverseProxyResource
-    # requestFactory = OAuthReverseProxyRequest
+	# TODO this may be the only required line, if validation occurs in OAuthReverseProxyResource
+	# requestFactory = OAuthReverseProxyRequest
 
 	def __init__(self, validator):
-	    self.validator = validator
+		self.validator = validator
 		proxy.ReverseProxy.__init__(self)
 
 
@@ -77,35 +77,35 @@ class OAuthReverseProxy(proxy.ReverseProxy):
 
 
 class OAuthReverseProxyResource(proxy.ReverseProxyResource):
-    def __init__(self, host, port, path, reactor=reactor, validator=OAuthValidator):
-        self.validator = validator
-        proxy.ReverseProxyResource(self, host, port, path, reactor)
+	def __init__(self, host, port, path, reactor=reactor, validator=OAuthValidator):
+		self.validator = validator
+		proxy.ReverseProxyResource(self, host, port, path, reactor)
 
 
-    def getChild(self, path, request):
-        return OAuthReverseProxyResource(
-            self.host, self.port, self.path + '/' + urlquote(path, safe="", validator=self.validator))
+	def getChild(self, path, request):
+		return OAuthReverseProxyResource(
+			self.host, self.port, self.path + '/' + urlquote(path, safe="", validator=self.validator))
 
 
-    def render(self, request):
-        # get OAuth headers from request.received_headers['authorization']
-        
-        # remove OAuth headers
-        
-        # parse querystring and POST body for OAuth params
-        qs = urlparse.urlparse(request.uri)[4]
-        
-        # rewrite the path w/o OAuth params
-        
-        # validate signature
-        
-        valid = self.validator.validate()
-        
-        if valid:
-            response = proxy.ReverseProxyResource.render(self, request)
-        else:
-            # render an error message
-            # TODO return NOT_DONE_YET in order to write headers
-            response = "Invalid signature"
-        
-        return response
+	def render(self, request):
+		# get OAuth headers from request.received_headers['authorization']
+		
+		# remove OAuth headers
+		
+		# parse querystring and POST body for OAuth params
+		qs = urlparse.urlparse(request.uri)[4]
+		
+		# rewrite the path w/o OAuth params
+		
+		# validate signature
+		
+		valid = self.validator.validate()
+		
+		if valid:
+			response = proxy.ReverseProxyResource.render(self, request)
+		else:
+			# render an error message
+			# TODO return NOT_DONE_YET in order to write headers
+			response = "Invalid signature"
+		
+		return response
