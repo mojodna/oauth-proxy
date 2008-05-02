6632e8c1d0dbd6a3aef4b4d761074f78c11950a7
Seth Fitzsimmons <seth@mojodna.net>
2008-05-02 16:12:10 +0100

    flatten args before signing (pp at myelin-co-nz)

--- oauth_proxy/oauth_proxy.py
+++ oauth_proxy/oauth_proxy.py
@@ -21,151 +21,157 @@ from twisted.python import log, usage
 from zope.interface import implements, Interface
 
 class IOAuthCredentialProvider(Interface):
-	"""An OAuth credential provider"""
-	
-	def fetchCredentials():
-		"""Fetch credentials"""
+    """An OAuth credential provider"""
+    
+    def fetchCredentials():
+        """Fetch credentials"""
 
 
 class StaticOAuthCredentialProvider:
-	implements(IOAuthCredentialProvider)
-	
-	def __init__(self, credentials):
-		self.credentials = credentials
-	
-	def fetchCredentials(self):
-		return self.credentials
+    implements(IOAuthCredentialProvider)
+    
+    def __init__(self, credentials):
+        self.credentials = credentials
+    
+    def fetchCredentials(self):
+        return self.credentials
 
 
 class OAuthCredentials:
-	"""
-	A container for OAuth credentials
-	"""
-	def __init__(self, consumerKey, consumerSecret, token = None, tokenSecret = None, signatureMethod = oauth.OAuthSignatureMethod_HMAC_SHA1()):
-		self.oauthConsumer = oauth.OAuthConsumer(consumerKey, consumerSecret)
-		
-		if token is not None and tokenSecret is not None:
-			self.oauthToken = oauth.OAuthToken(token, tokenSecret)
-		else:
-			self.oauthToken = None
-			
-		self.signatureMethod = signatureMethod
+    """
+    A container for OAuth credentials
+    """
+    def __init__(self, consumerKey, consumerSecret, token = None, tokenSecret = None, signatureMethod = oauth.OAuthSignatureMethod_HMAC_SHA1()):
+        self.oauthConsumer = oauth.OAuthConsumer(consumerKey, consumerSecret)
+        
+        if token is not None and tokenSecret is not None:
+            self.oauthToken = oauth.OAuthToken(token, tokenSecret)
+        else:
+            self.oauthToken = None
+            
+        self.signatureMethod = signatureMethod
 
 
 class Options(usage.Options):
-	synopsis = "Usage: oauth_proxy --consumer-key <consumer key> --consumer-secret <consumer secret> [--token <token>] [--token-secret <token secret>] [-p <proxy port>] [--ssl]"
-	longdesc = "An OAuth HTTP proxy server.."
-	optParameters = [
-		['consumer-key', None, None, "OAuth Consumer Key"],
-		['consumer-secret', None, None, "OAuth Consumer Secret"],
-		['token', None, None, "OAuth Access/Request Token"],
-		['token-secret', None, None, "OAuth Access/Request Token Secret"],
-		['port', 'p', 8001, "Proxy port", int],
-	]
+    synopsis = "Usage: oauth_proxy --consumer-key <consumer key> --consumer-secret <consumer secret> [--token <token>] [--token-secret <token secret>] [-p <proxy port>] [--ssl]"
+    longdesc = "An OAuth HTTP proxy server.."
+    optParameters = [
+        ['consumer-key', None, None, "OAuth Consumer Key"],
+        ['consumer-secret', None, None, "OAuth Consumer Secret"],
+        ['token', None, None, "OAuth Access/Request Token"],
+        ['token-secret', None, None, "OAuth Access/Request Token Secret"],
+        ['port', 'p', 8001, "Proxy port", int],
+    ]
 
-	optFlags = [['ssl', 's']]
+    optFlags = [['ssl', 's']]
 
 
 class OAuthProxyClient(proxy.ProxyClient):
-	def connectionMade(self):
-		# if retrieval of OAuth credentials is to be asynchronous, it needs to be done here (if it's even possible)
-		# otherwise, this class has no point
-		# however, it's possible that reading headers can't happen in OAuthProxyClientFactory
-		proxy.ProxyClient.connectionMade(self)
+    def connectionMade(self):
+        # if retrieval of OAuth credentials is to be asynchronous, it needs to be done here (if it's even possible)
+        # otherwise, this class has no point
+        # however, it's possible that reading headers can't happen in OAuthProxyClientFactory
+        proxy.ProxyClient.connectionMade(self)
 
 
 class OAuthProxyClientFactory(proxy.ProxyClientFactory):
-	def buildProtocol(self, addr):
-		credentials = self.father.credentialProvider.fetchCredentials()
-		oauthRequest = self.signRequest(credentials)
-		
-		client = proxy.ProxyClientFactory.buildProtocol(self, addr)
-		# upgrade proxy.proxyClient object to OAuthProxyClient
-		client.__class__ = OAuthProxyClient
-		client.factory = self
-		
-		client.headers.update(oauthRequest.to_header())
-		return client
+    def buildProtocol(self, addr):
+        credentials = self.father.credentialProvider.fetchCredentials()
+        oauthRequest = self.signRequest(credentials)
+        
+        client = proxy.ProxyClientFactory.buildProtocol(self, addr)
+        # upgrade proxy.proxyClient object to OAuthProxyClient
+        client.__class__ = OAuthProxyClient
+        client.factory = self
+        
+        client.headers.update(oauthRequest.to_header())
+        return client
 
-	def signRequest(self, credentials):
-		"""Create an OAuthRequest and sign it"""
-		
-		if self.father.useSSL:
-			path = self.father.path.replace("http", "https", 1)
-		else:
-			path = self.father.path
+    def signRequest(self, credentials):
+        """Create an OAuthRequest and sign it"""
+        
+        if self.father.useSSL:
+            path = self.father.path.replace("http", "https", 1)
+        else:
+            path = self.father.path
 
-		# create an OAuth Request from the pieces that we've assembled
-		oauthRequest = oauth.OAuthRequest.from_consumer_and_token(
-			credentials.oauthConsumer,
-			credentials.oauthToken,
-			self.father.method,
-			path,
-			self.father.args
-		)
+        # python parses arguments into a dict of arrays, e.g. 'q=foo' becomes {'q': ['foo']}
+        # while from_consumer_and_token expects a dict of strings, so we cross our fingers,
+        # hope there are no repeated arguments ('q=foo&q=bar'), and take the first value of
+        # each array.
+        args = dict([(k,v[0]) for k,v in self.father.args.items()])
 
-		# now, sign it
-		oauthRequest.sign_request(credentials.signatureMethod, credentials.oauthConsumer, credentials.oauthToken)
-	    
-		return oauthRequest
+        # create an OAuth Request from the pieces that we've assembled
+        oauthRequest = oauth.OAuthRequest.from_consumer_and_token(
+            credentials.oauthConsumer,
+            credentials.oauthToken,
+            self.father.method,
+            path,
+            args
+        )
+
+        # now, sign it
+        oauthRequest.sign_request(credentials.signatureMethod, credentials.oauthConsumer, credentials.oauthToken)
+        
+        return oauthRequest
 
 
 class OAuthProxyRequest(proxy.ProxyRequest):
-	protocols = {'http': OAuthProxyClientFactory}
+    protocols = {'http': OAuthProxyClientFactory}
 
-	def __init__(self, credentialProvider, useSSL, *args):
-		self.credentialProvider = credentialProvider
-		self.useSSL = useSSL
-		proxy.ProxyRequest.__init__(self, *args)
-		
-		if self.useSSL:
-			# Since we magically mapped HTTP to HTTPS, we want to make sure that the transport knows as much
-			self._forceSSL = True
-			self.ports["http"] = 443
+    def __init__(self, credentialProvider, useSSL, *args):
+        self.credentialProvider = credentialProvider
+        self.useSSL = useSSL
+        proxy.ProxyRequest.__init__(self, *args)
+        
+        if self.useSSL:
+            # Since we magically mapped HTTP to HTTPS, we want to make sure that the transport knows as much
+            self._forceSSL = True
+            self.ports["http"] = 443
 
-	# Copied from proxy.ProxyRequest just so the reactor connection can be SSL
-	def process(self):
-		parsed = urlparse.urlparse(self.uri)
-		protocol = parsed[0]
-		host = parsed[1]
-		port = self.ports[protocol]
-		if ':' in host:
-			host, port = host.split(':')
-			port = int(port)
-		rest = urlparse.urlunparse(('', '') + parsed[2:])
-		if not rest:
-			rest = rest + '/'
-		class_ = self.protocols[protocol]
-		headers = self.getAllHeaders().copy()
-		if 'host' not in headers:
-			headers['host'] = host
-		self.content.seek(0, 0)
-		s = self.content.read()
-		clientFactory = class_(self.method, rest, self.clientproto, headers,
-							   s, self)
-		# The magic line for SSL support!
-		if self.useSSL:
-			self.reactor.connectSSL(host, port, clientFactory, ssl.ClientContextFactory())
-		else:
-			self.reactor.connectTCP(host, port, clientFactory)
+    # Copied from proxy.ProxyRequest just so the reactor connection can be SSL
+    def process(self):
+        parsed = urlparse.urlparse(self.uri)
+        protocol = parsed[0]
+        host = parsed[1]
+        port = self.ports[protocol]
+        if ':' in host:
+            host, port = host.split(':')
+            port = int(port)
+        rest = urlparse.urlunparse(('', '') + parsed[2:])
+        if not rest:
+            rest = rest + '/'
+        class_ = self.protocols[protocol]
+        headers = self.getAllHeaders().copy()
+        if 'host' not in headers:
+            headers['host'] = host
+        self.content.seek(0, 0)
+        s = self.content.read()
+        clientFactory = class_(self.method, rest, self.clientproto, headers,
+                               s, self)
+        # The magic line for SSL support!
+        if self.useSSL:
+            self.reactor.connectSSL(host, port, clientFactory, ssl.ClientContextFactory())
+        else:
+            self.reactor.connectTCP(host, port, clientFactory)
 
 
 class OAuthProxy(proxy.Proxy):
-	def __init__(self, credentialProvider, useSSL):
-		self.credentialProvider = credentialProvider
-		self.useSSL = useSSL
-		proxy.Proxy.__init__(self)
+    def __init__(self, credentialProvider, useSSL):
+        self.credentialProvider = credentialProvider
+        self.useSSL = useSSL
+        proxy.Proxy.__init__(self)
 
-	def requestFactory(self, *args):
-		return OAuthProxyRequest(self.credentialProvider, self.useSSL, *args)
+    def requestFactory(self, *args):
+        return OAuthProxyRequest(self.credentialProvider, self.useSSL, *args)
 
 
 class OAuthProxyFactory(http.HTTPFactory):
-	def __init__(self, credentialProvider, useSSL):
-		self.credentialProvider = credentialProvider
-		self.useSSL = useSSL
-		http.HTTPFactory.__init__(self)
+    def __init__(self, credentialProvider, useSSL):
+        self.credentialProvider = credentialProvider
+        self.useSSL = useSSL
+        http.HTTPFactory.__init__(self)
 
-	def buildProtocol(self, addr):
-		protocol = OAuthProxy(self.credentialProvider, self.useSSL)
-		return protocol
+    def buildProtocol(self, addr):
+        protocol = OAuthProxy(self.credentialProvider, self.useSSL)
+        return protocol
